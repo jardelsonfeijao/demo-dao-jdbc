dd4aeb7ecaf57a27e2cf608e054a4ea9d8fb61be
Jardelson Feijao <jardelsonfeijao04@gmail.com>
2022-03-29 17:01:53 -0300

    FindById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoImplJDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
-		return new SellerDaoImplJDBC();
+		return new SellerDaoImplJDBC(DB.getConnection());
 	}
 }
